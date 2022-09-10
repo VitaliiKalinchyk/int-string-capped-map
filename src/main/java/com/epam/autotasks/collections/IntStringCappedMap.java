6cebc73e91249f8a9d70cfb5fb21775ba4bf3891
Vitalii Kalinchyk <karlkory@gmail.com>
2022-09-10 10:49:42 +0300

    done

--- src/main/java/com/epam/autotasks/collections/IntStringCappedMap.java
+++ src/main/java/com/epam/autotasks/collections/IntStringCappedMap.java
@@ -1,15 +1,10 @@
 package com.epam.autotasks.collections;
 
-import java.util.AbstractMap;
-import java.util.AbstractSet;
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 class IntStringCappedMap extends AbstractMap<Integer, String> {
+
+    ArrayList<Entry<Integer, String>> list = new ArrayList<>();
 
     private final long capacity;
 
@@ -27,16 +22,15 @@ class IntStringCappedMap extends AbstractMap<Integer, String> {
             @Override
             public Iterator<Entry<Integer, String>> iterator() {
                 return new Iterator<>() {
+                    int cursor = 0;
                     @Override
                     public boolean hasNext() {
-                        //implement this method
-                        throw new UnsupportedOperationException();
+                        return cursor != list.size();
                     }
 
                     @Override
                     public Entry<Integer, String> next() {
-                        //implement this method
-                        throw new UnsupportedOperationException();
+                        return list.get(cursor++);
                     }
                 };
             }
@@ -50,26 +44,53 @@ class IntStringCappedMap extends AbstractMap<Integer, String> {
 
     @Override
     public String get(final Object key) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        String result = null;
+        for (Entry<Integer, String> entry : entrySet()) {
+            if (entry.getKey().equals(key)) {
+                result = entry.getValue();
+                break;
+            }
+        }
+        return result;
     }
 
     @Override
     public String put(final Integer key, final String value) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        if (value.length() > capacity) throw new IllegalArgumentException();
+
+        String result = get(key);
+        remove(key);
+        SimpleEntry<Integer, String> current = new SimpleEntry<>(key, value);
+        list.add(current);
+        while (currentCapacity() > capacity) {
+            list.remove(0);
+        }
+        return result;
     }
 
     @Override
     public String remove(final Object key) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        String result = null;
+        for (Entry<Integer, String> entry : entrySet()) {
+            if (entry.getKey().equals(key)) {
+                result = entry.getValue();
+                list.remove(entry);
+                break;
+            }
+        }
+        return result;
     }
 
     @Override
     public int size() {
-        //implement this method
-        throw new UnsupportedOperationException();
+        return list.size();
     }
 
+    private long currentCapacity() {
+        long currentCapacity = 0;
+        for (Entry<Integer, String> entry : entrySet()) {
+            currentCapacity += entry.getValue().length();
+        }
+        return currentCapacity;
+    }
 }
